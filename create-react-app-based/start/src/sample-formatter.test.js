56de7c119f9e26e047fdee40fff56ae2efeb1bec
Braulio Diez Botella <braulio.diez@gmail.com>
2019-06-28 18:07:19 +0200

    start sample prepared

---
+++ create-react-app-based/start/src/sample-formatter.test.js
@@ -0,0 +1,12 @@
+import { formatCurrency } from "./sample-formatter";
+
+it("Display a number in es-es currencty format", () => {
+  // Arrange
+  const value = 200;
+
+  // Act
+  const formattedText = formatCurrency(value);
+
+  // Assert
+  expect(formattedText).toEqual("200,00 €");
+});
